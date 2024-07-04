f30ab86825ae26c5eea437381f8fe0ee65b0ede0
Douglas J. Lee <46190860+dlee242@users.noreply.github.com>
2024-07-04 23:10:02 +0000

    chore: moved github to async

---
+++ github.js
@@ -0,0 +1,24 @@
+
+import { getInput, setOutput, setFailed } from '@actions/core';
+import { getOctokit } from '@actions/github';
+
+export async function run() {
+
+  // This should be a token with access to your repository scoped in as a secret.
+  // The YML workflow will need to set myToken with the GitHub Secret Token
+  // myToken: ${{ secrets.GITHUB_TOKEN }}
+  // https://help.github.com/en/actions/automating-your-workflow-with-github-actions/authenticating-with-the-github_token#about-the-github_token-secret
+  const myToken = getInput('PAT');
+  
+  const octokit = getOctokit(myToken)
+
+  // https://octokit.github.io/rest.js/v20#repos-get-content
+  const repoContent = octokit.rest.repos.getContent({
+    owner: "dlee242",
+    repo: "gha-test-repo",
+    path: "",
+  });
+
+  console.log(repoContent);
+
+}
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import { getInput, setOutput, setFailed } from '@actions/core';
-import { context, getOctokit } from '@actions/github';
-
+import { run } from './github';
 
 try {
   // `who-to-greet` input defined in action metadata file
@@ -12,26 +11,8 @@ try {
 
   const time = (new Date()).toTimeString();
   setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  run();
 
-  // This should be a token with access to your repository scoped in as a secret.
-  // The YML workflow will need to set myToken with the GitHub Secret Token
-  // myToken: ${{ secrets.GITHUB_TOKEN }}
-  // https://help.github.com/en/actions/automating-your-workflow-with-github-actions/authenticating-with-the-github_token#about-the-github_token-secret
-  const myToken = getInput('PAT');
-  
-  const octokit = getOctokit(myToken)
-  
-  const repoContent = octokit.rest.repos.getContent({
-    owner: "dlee242",
-    repo: "gha-test-repo",
-    path: "",
-  });
-
-  console.log(repoContent);
-  
 } catch (error) {
   setFailed(error.message);
 }
