03c4a377a79e88d7e50d874b8aa78827b2a456a9
Douglas J. Lee <46190860+dlee242@users.noreply.github.com>
2024-07-04 22:03:44 +0000

    build: refresh dist

--- dist/index.js
+++ dist/index.js
@@ -30935,6 +30935,10 @@ try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = (0,_actions_core__WEBPACK_IMPORTED_MODULE_0__.getInput)('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
+  
+  const testInput = (0,_actions_core__WEBPACK_IMPORTED_MODULE_0__.getInput)('test-input');
+  console.log(`Test Input: ${testInput}`);
+
   const time = (new Date()).toTimeString();
   (0,_actions_core__WEBPACK_IMPORTED_MODULE_0__.setOutput)("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
