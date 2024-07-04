02dc54887caac2fcca90f1cc82100aea423bcb74
Douglas J. Lee <46190860+dlee242@users.noreply.github.com>
2024-07-04 22:59:30 +0000

    chore: test github package

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { getInput, setOutput, setFailed } from '@actions/core';
 import { context } from '@actions/github';
 
+
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = getInput('who-to-greet');
@@ -14,6 +15,30 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
+
+  // This should be a token with access to your repository scoped in as a secret.
+  // The YML workflow will need to set myToken with the GitHub Secret Token
+  // myToken: ${{ secrets.GITHUB_TOKEN }}
+  // https://help.github.com/en/actions/automating-your-workflow-with-github-actions/authenticating-with-the-github_token#about-the-github_token-secret
+  const myToken = core.getInput('PAT');
+  
+  const octokit = github.getOctokit(myToken)
+  
+  const repoContent = octokit.rest.repos.getContent({
+    owner: "dlee242",
+    repo: "gha-test-repo",
+    path: "",
+  });
+
+  console.log(repoContent);
+  
+  // You can also pass in additional options as a second parameter to getOctokit
+  // const octokit = github.getOctokit(myToken, {userAgent: "MyActionVersion1"});
+  
+  
+
+  console.log(pullRequest);
+  
 } catch (error) {
   setFailed(error.message);
 }
