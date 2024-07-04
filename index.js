11c06be9830b1181258fb8c543b43eafa1b9fe28
Douglas J. Lee <46190860+dlee242@users.noreply.github.com>
2024-07-04 22:03:10 +0000

    testing custom input

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
+  
+  const testInput = getInput('test-input');
+  console.log(`Test Input: ${testInput}`);
+
   const time = (new Date()).toTimeString();
   setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
