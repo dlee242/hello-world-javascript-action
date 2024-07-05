24941f5d9ea275bf23d7eaa0c79a980a1224b395
Douglas J. Lee <46190860+dlee242@users.noreply.github.com>
2024-07-05 00:33:41 +0000

    chore: test await sodium

--- utils.js
+++ utils.js
@@ -3,15 +3,16 @@ import { setSecret } from "@actions/core";
 import * as sodium from 'libsodium-wrappers';
 
 
-export function encrypt(value, key) {
-    const binkey = sodium.from_base64(key, sodium.base64_variants.ORIGINAL)
-    const binsec = sodium.from_string(value)
+export async function encrypt(value, key) {
+    await sodium.ready;
+    const binkey = sodium.from_base64(key, sodium.base64_variants.ORIGINAL);
+    const binsec = sodium.from_string(value);
   
     // Encrypt the secret using libsodium
-    const encryptedBytes = sodium.crypto_box_seal(binsec, binkey)
+    const encryptedBytes = sodium.crypto_box_seal(binsec, binkey);
   
     // Convert the encrypted Uint8Array to Base64
-    const encrypted = sodium.to_base64(encryptedBytes, sodium.base64_variants.ORIGINAL)
+    const encrypted = sodium.to_base64(encryptedBytes, sodium.base64_variants.ORIGINAL);
 
     // tell Github to mask this from logs
     setSecret(encrypted);
