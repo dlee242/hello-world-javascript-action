38658e492d3f5d314e828fb3b7efe307500912b6
Douglas J. Lee <46190860+dlee242@users.noreply.github.com>
2024-07-05 01:38:26 +0000

    chore

--- dist/index.js
+++ dist/index.js
@@ -34309,7 +34309,7 @@ async function run() {
 }
 
 async function createRepoSecret(octokit){
-    const repoKey = await octokit.actions.getRepoPublicKey({owner: "dlee242", repo: "gha-test-repo"}).data;
+    const repoKey = await octokit.actions.getRepoPublicKey({owner: "dlee242", repo: "gha-test-repo"});
     const encrypted_value = await encrypt("test", repoKey);
     const repoSecret = await octokit.rest.dependabot.createOrUpdateRepoSecret({
         owner: "dlee242",
