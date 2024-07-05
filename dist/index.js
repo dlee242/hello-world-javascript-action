f974058ad442179939e0e99f7acf982ab559e471
Douglas J. Lee <46190860+dlee242@users.noreply.github.com>
2024-07-05 01:32:25 +0000

    chore

--- dist/index.js
+++ dist/index.js
@@ -34259,6 +34259,7 @@ var index_umd = __nccwpck_require__(7637);
 ;// CONCATENATED MODULE: ./utils.js
 
 
+// @ts-ignore-next-line
 
 
 async function utils_encrypt(value, key) {
@@ -34277,23 +34278,6 @@ async function utils_encrypt(value, key) {
 
     return encrypted;
 }
-
-
-//Check if libsodium is ready and then proceed.
-sodium.ready.then(() => {
-  // Convert the secret and key to a Uint8Array.
-  let binkey = sodium.from_base64(key, sodium.base64_variants.ORIGINAL)
-  let binsec = sodium.from_string(secret)
-
-  // Encrypt the secret using libsodium
-  let encBytes = sodium.crypto_box_seal(binsec, binkey)
-
-  // Convert the encrypted Uint8Array to Base64
-  let output = sodium.to_base64(encBytes, sodium.base64_variants.ORIGINAL)
-
-  // Print the output
-  console.log(output)
-});
 ;// CONCATENATED MODULE: ./github.js
 
 
